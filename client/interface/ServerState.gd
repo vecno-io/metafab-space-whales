0377afd27b38da737e60f36dea24c3e58c715c71
Koen Van Rulo <koen.vr@vecno.io>
2023-01-21 20:55:00 +0100

    tweak status button
    * replaces status text

--- client/interface/ServerState.gd
+++ client/interface/ServerState.gd
@@ -1,9 +1,25 @@
 extends Node
 
+export(Texture) var stats_h_a: Texture
+export(Texture) var stats_h_b: Texture
+export(Texture) var stats_h_c: Texture
+export(Texture) var stats_h_d: Texture
+
+export(Texture) var stats_n_a: Texture
+export(Texture) var stats_n_b: Texture
+export(Texture) var stats_n_c: Texture
+export(Texture) var stats_n_d: Texture
+
+export(Texture) var stats_p_a: Texture
+export(Texture) var stats_p_b: Texture
+export(Texture) var stats_p_c: Texture
+export(Texture) var stats_p_d: Texture
 
 var manager: MainOverlay = null
 
 onready var info_box = get_node("%StateInfoBox")
+
+onready var status = get_node("%StatusBtn")
 
 onready var actor_label = get_node("%ActorLabel")
 onready var account_label = get_node("%AccountLabel")
@@ -16,26 +32,54 @@ func _ready():
 
 func actor_loaded():
 	actor_label.text = "Ok - Actor"
+	status.texture_hover = stats_h_d
+	status.texture_pressed = stats_p_d
+	status.texture_normal = stats_n_d
+	status.texture_focused = stats_n_d
+	status.texture_disabled = stats_n_d
 
 
 func actor_cleared():
 	actor_label.text = "None - Actor"
+	status.texture_hover = stats_h_c
+	status.texture_pressed = stats_p_c
+	status.texture_normal = stats_n_c
+	status.texture_focused = stats_n_c
+	status.texture_disabled = stats_n_c
 
 
 func signed_in():
 	account_label.text = "Ok - Account"
-
+	status.texture_hover = stats_h_c
+	status.texture_pressed = stats_p_c
+	status.texture_normal = stats_n_c
+	status.texture_focused = stats_n_c
+	status.texture_disabled = stats_n_c
 
 func signed_out():
 	account_label.text = "None - Account"
-
+	status.texture_hover = stats_h_b
+	status.texture_pressed = stats_p_b
+	status.texture_normal = stats_n_b
+	status.texture_focused = stats_n_b
+	status.texture_disabled = stats_n_b
 
 func session_created():
 	connected_label.text = "Ok - Connected"
+	status.texture_hover = stats_h_b
+	status.texture_pressed = stats_p_b
+	status.texture_normal = stats_n_b
+	status.texture_focused = stats_n_b
+	status.texture_disabled = stats_n_b
 
 
 func session_closed():
 	connected_label.text = "None - Connected"
+	status.texture_hover = stats_h_a
+	status.texture_pressed = stats_p_a
+	status.texture_normal = stats_n_a
+	status.texture_focused = stats_n_a
+	status.texture_disabled = stats_n_a
 
 
 func _on_mouse_exited():

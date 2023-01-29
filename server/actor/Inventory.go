e7dd988c7fe4446e3b4cc5c0d6dc90261127aeab
Koen Van Rulo <koen.vr@vecno.io>
2023-01-29 15:42:30 +0100

    init combat outcome
    * simplified no verification

--- server/actor/Inventory.go
+++ server/actor/Inventory.go
@@ -59,6 +59,12 @@ type InvemtoryRequest struct {
 	Actor string `json:"actor"`
 }
 
+type CombatOutcome struct {
+	Actor string `json:"actor"`
+	Dust  uint64 `json:"dust"`
+	Speed  uint64 `json:"speed"`
+	Attack  uint64 `json:"attack"`
+}
 
 func (res *CoinResult) FromRequest(req CoinRequest) {
 	res.Key = req.Key
@@ -93,7 +99,7 @@ func InventoryRpc(ctx context.Context, logger runtime.Logger, db *sql.DB, nk run
 			"code": 500,
 			"user": user_id,
 			"data": payload,
-		}).Error("unmarshal create request")
+		}).Error("unmarshal request")
 		return "{}", runtime.NewError("invalid request", 500)
 	}
 	// TODO NEXT -> Player needs to own the actor
@@ -187,6 +193,149 @@ func InventoryRpc(ctx context.Context, logger runtime.Logger, db *sql.DB, nk run
 	return string(data), nil
 }
 
+func CombatOutcomeRpc(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+	// Requires a valid metafab account, that account needs to own the actor
+	user_id, account, err := meta.GetAccountData(ctx, logger, nk)
+	if nil != err {
+		logger.WithFields(map[string]interface{}{
+			"err": err,
+			"user": user_id,
+		}).Error("failed to get acount data")
+		return "{}", errors.New("failed to load account data")
+	}
+	var request CombatOutcome 
+	if err := json.Unmarshal([]byte(payload), &request); err != nil {
+		logger.WithFields(map[string]interface{}{
+			"err": err,
+			"code": 500,
+			"user": user_id,
+			"data": payload,
+		}).Error("unmarshal request")
+		return "{}", runtime.NewError("invalid request", 500)
+	}
+	// TODO NEXT -> Player needs to own the actor
+	// METAFAB API CALL: Get collection item balance
+	// Read coin values for player (storage) and actor (inventory)
+	// ===============================
+	// Read Inventory state for actor
+	var result = InvemtoryResult{}
+	result.Actor = request.Actor
+	// FixMe: Hardcoded coin and booster keys
+	dust_stored, _, err := _read_coin_for_storage(ctx, logger, nk, user_id, "DUST")
+	if nil != err {
+		logger.WithFields(map[string]interface{}{
+			"err": err,
+			"key": "DUST",
+			"user": user_id,
+			"player": account.MetafabId,
+		}).Error("read storage (user)")
+		return "{}", errors.New("read storage failed")
+	}
+	dust_inventory, dust_version, err := _read_coin_for_inventory(ctx, logger, nk, request.Actor, "DUST")
+	if nil != err {
+		logger.WithFields(map[string]interface{}{
+			"err": err,
+			"key": "DUST",
+			"user": user_id,
+			"player": account.MetafabId,
+		}).Error("read storage (account)")
+		return "{}", errors.New("read storage failed")
+	}
+	speed_stored, _, err := _read_booster_for_storage(ctx, logger, nk, user_id, "SPEED")
+	if nil != err {
+		logger.WithFields(map[string]interface{}{
+			"err": err,
+			"key": "SPEED",
+			"user": user_id,
+			"player": account.MetafabId,
+		}).Error("read storage (user)")
+		return "{}", errors.New("read storage failed")
+	}
+	speed_inventory, speed_version, err := _read_booster_for_inventory(ctx, logger, nk, request.Actor, "SPEED")
+	if nil != err {
+		logger.WithFields(map[string]interface{}{
+			"err": err,
+			"key": "SPEED",
+			"user": user_id,
+			"player": account.MetafabId,
+		}).Error("read storage (account)")
+		return "{}", errors.New("read storage failed")
+	}
+	attack_stored, _, err := _read_booster_for_storage(ctx, logger, nk, user_id, "ATTACK")
+	if nil != err {
+		logger.WithFields(map[string]interface{}{
+			"err": err,
+			"key": "ATTACK",
+			"user": user_id,
+			"player": account.MetafabId,
+		}).Error("read storage (user)")
+		return "{}", errors.New("read storage failed")
+	}
+	attack_inventory, attack_version, err := _read_booster_for_inventory(ctx, logger, nk, request.Actor, "ATTACK")
+	if nil != err {
+		logger.WithFields(map[string]interface{}{
+			"err": err,
+			"key": "ATTACK",
+			"user": user_id,
+			"player": account.MetafabId,
+		}).Error("read storage (account)")
+		return "{}", errors.New("read storage failed")
+	}
+	// Update all values and set result
+	// TODO: Validate based on combat logs
+	dust_inventory.Value = request.Dust
+	speed_inventory.Value = request.Speed
+	attack_inventory.Value = request.Attack
+	result.Coins = []CoinState{{
+		Key: "DUST",
+		Stored: dust_stored.Value,
+		Inventory: dust_inventory.Value,
+	}}
+	result.Boosters = []BoosterState{{
+		Type: "SPEED",
+		Stored: speed_stored.Value,
+		Inventory: speed_inventory.Value,
+	},{
+		Type: "ATTACK",
+		Stored: attack_stored.Value,
+		Inventory: attack_inventory.Value,
+	}}
+	if _, err := _write_coin_for_inventory(ctx, logger, nk, request.Actor, "DUST", dust_inventory, dust_version); nil != err {
+		logger.WithFields(map[string]interface{}{
+			"err": err,
+			"user": user_id,
+			"player": account.MetafabId,
+			"payload": payload,
+		}).Error("write coin inventory (DUST)")
+		return "{}", errors.New("write coin inventory failed")
+	}
+	if _, err := _write_booster_for_inventory(ctx, logger, nk, request.Actor, "SPEED", speed_inventory, speed_version); nil != err {
+		logger.WithFields(map[string]interface{}{
+			"err": err,
+			"user": user_id,
+			"player": account.MetafabId,
+			"payload": payload,
+		}).Error("write booster inventory (SPEED)")
+		return "{}", errors.New("write booster inventory failed")
+	}
+	if _, err := _write_booster_for_inventory(ctx, logger, nk, request.Actor, "ATTACK", attack_inventory, attack_version); nil != err {
+		logger.WithFields(map[string]interface{}{
+			"err": err,
+			"user": user_id,
+			"player": account.MetafabId,
+			"payload": payload,
+		}).Error("write booster inventory (ATTACK)")
+		return "{}", errors.New("write booster inventory failed")
+	}
+	// Finalize result and send out
+	data, err := json.Marshal(result)
+	if err != nil {
+		logger.WithField("err", err).Error("json marshal result")
+		return "{}", errors.New("json marshal result failed")
+	}
+	return string(data), nil
+}
+
 
 func TakeCoinRpc(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
 	// Requires a valid metafab account, that account needs to own the actor
@@ -205,7 +354,7 @@ func TakeCoinRpc(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runt
 			"code": 500,
 			"user": user_id,
 			"data": payload,
-		}).Error("unmarshal create request")
+		}).Error("unmarshal request")
 		return "{}", runtime.NewError("invalid request", 500)
 	}
 	// TODO NEXT -> Player needs to own the actor
@@ -308,7 +457,7 @@ func StoreCoinRpc(ctx context.Context, logger runtime.Logger, db *sql.DB, nk run
 			"code": 500,
 			"user": user_id,
 			"data": payload,
-		}).Error("unmarshal create request")
+		}).Error("unmarshal request")
 		return "{}", runtime.NewError("invalid request", 500)
 	}
 	// TODO NEXT -> Player needs to own the actor
@@ -411,7 +560,7 @@ func TakeBoosterRpc(ctx context.Context, logger runtime.Logger, db *sql.DB, nk r
 			"code": 500,
 			"user": user_id,
 			"data": payload,
-		}).Error("unmarshal create request")
+		}).Error("unmarshal request")
 		return "{}", runtime.NewError("invalid request", 500)
 	}
 	// TODO NEXT -> Player needs to own the actor
@@ -514,7 +663,7 @@ func StoreBoosterRpc(ctx context.Context, logger runtime.Logger, db *sql.DB, nk 
 			"code": 500,
 			"user": user_id,
 			"data": payload,
-		}).Error("unmarshal create request")
+		}).Error("unmarshal request")
 		return "{}", runtime.NewError("invalid request", 500)
 	}
 	// TODO NEXT -> Player needs to own the actor
@@ -740,7 +889,6 @@ func _write_booster_for_inventory(ctx context.Context, logger runtime.Logger, nk
 	}
 	return ack[0].Version, nil
 }
-
 
 
 func _read_coin_for_storage(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, user_id string, key string) (*Coin, string, error) {
--- server/main.go
+++ server/main.go
@@ -25,6 +25,7 @@ func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runti
 		logger.Error("Unable to register rpc: meta_register: %v", err)
 		return err
 	}
+
 	if err := initializer.RegisterRpc("actor_mint", actor.MintRpc); err != nil {
 		logger.Error("Unable to register rpc: actor_mint: %v", err)
 		return err
@@ -37,24 +38,30 @@ func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runti
 		logger.Error("Unable to register rpc: actor_reserve: %v", err)
 		return err
 	}
+
 	if err := initializer.RegisterRpc("actor_inventory", actor.InventoryRpc); err != nil {
-		logger.Error("Unable to register rpc: actor_reserve: %v", err)
+		logger.Error("Unable to register rpc: actor_inventory: %v", err)
 		return err
 	}
 	if err := initializer.RegisterRpc("actor_take_coin", actor.TakeCoinRpc); err != nil {
-		logger.Error("Unable to register rpc: actor_reserve: %v", err)
+		logger.Error("Unable to register rpc: actor_take_coin: %v", err)
 		return err
 	}
 	if err := initializer.RegisterRpc("actor_store_coin", actor.StoreCoinRpc); err != nil {
-		logger.Error("Unable to register rpc: actor_reserve: %v", err)
+		logger.Error("Unable to register rpc: actor_store_coin: %v", err)
 		return err
 	}
 	if err := initializer.RegisterRpc("actor_take_booster", actor.TakeBoosterRpc); err != nil {
-		logger.Error("Unable to register rpc: actor_reserve: %v", err)
+		logger.Error("Unable to register rpc: actor_take_booster: %v", err)
 		return err
 	}
 	if err := initializer.RegisterRpc("actor_store_booster", actor.StoreBoosterRpc); err != nil {
-		logger.Error("Unable to register rpc: actor_reserve: %v", err)
+		logger.Error("Unable to register rpc: actor_store_booster: %v", err)
+		return err
+	}
+
+	if err := initializer.RegisterRpc("actor_combat_outcome", actor.CombatOutcomeRpc); err != nil {
+		logger.Error("Unable to register rpc: actor_combat_outcome: %v", err)
 		return err
 	}
 	return nil
